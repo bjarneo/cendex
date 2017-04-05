15342142a4ec44d9376d31f30d529a70f1a8ef7f
bjarneo <bjarne.overli@vg.no>
2017-04-05 20:34:42 +0200

    update the test to check for every element in the array to ensure it works as expected

--- index.test.js
+++ index.test.js
@@ -9,7 +9,11 @@ const arr = [1, 2, 3, 4, 5, 6, 7];
 describe('#cendex', () => {
     it('should return the correct index from an array', () => {
         assert.strictEqual(cendex(arr, 1), 0);
+        assert.strictEqual(cendex(arr, 2), 1);
+        assert.strictEqual(cendex(arr, 3), 2);
+        assert.strictEqual(cendex(arr, 4), 3);
         assert.strictEqual(cendex(arr, 5), 4);
+        assert.strictEqual(cendex(arr, 6), 5);
         assert.strictEqual(cendex(arr, 7), 6);
     });
 
